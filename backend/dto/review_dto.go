df4702cce68b36d56a99892ed66598f0f4999d33
Dehendy Wijaya <143471727+dehendywijay@users.noreply.github.com>
2026-04-28 22:00:31 +0700

    feat:add type reviewrespone

--- backend/dto/review_dto.go
+++ backend/dto/review_dto.go
@@ -6,3 +6,11 @@ type CreateReviewRequest struct {
 	Rating  int    `json:"rating" binding:"required,min=1,max=5"`
 	Comment string `json:"comment" binding:"required"`
 }
+
+type ReviewResponse struct {
+	ID       uint   `json:"id"`
+	UserID   uint   `json:"user_id"`
+	WisataID uint   `json:"wisata_id"`
+	Rating   int    `json:"rating"`
+	Comment  string `json:"comment"`
+}
