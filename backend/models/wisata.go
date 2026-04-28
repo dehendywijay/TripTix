d461c708e685ec274b08df6c736c8044789e9ed0
Dehendy Wijaya <143471727+dehendywijay@users.noreply.github.com>
2026-04-28 15:47:57 +0700

    minor:add user

--- backend/models/wisata.go
+++ backend/models/wisata.go
@@ -2,6 +2,16 @@ package models
 
 import "gorm.io/gorm"
 
+
+type User struct {
+	gorm.Model
+	Nama     string `json:"nama" gorm:"not null"`
+	Email    string `json:"email" gorm:"not null"`
+	Password string `json:"password" gorm:"not null"`
+	
+	Reviews  []Review `gorm:"foreignKey:UserID"`
+}
+
 type Wisata struct {
 	gorm.Model
 	Nama    string `json:"nama" gorm:"not null"`
