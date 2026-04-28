870d051b61cebe7dc6364732ca114fbb499ec4d0
Dehendy Wijaya <143471727+dehendywijay@users.noreply.github.com>
2026-04-28 15:58:13 +0700

    feat:add controller for Register user

---
+++ backend/controllers/auth_controllers.go
@@ -0,0 +1,35 @@
+package controller
+
+import (
+	"net/http"
+	"triptix/models"
+	"triptix/services"
+	"triptix/utils"
+	"github.com/gin-gonic/gin"
+)
+
+func RegisterUser(c *gin.Context) {
+	var user models.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	hashedPassword, err := utils.HashPassword(user.Password)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	user.Password = hashedPassword
+	user, err = services.RegisterUser(user)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{
+		"message": "User created successfully",
+		"data":    user,
+	})
+	}
