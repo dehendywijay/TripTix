e501e43dd308619dc090f825d3caef9af53c7ec9
Dehendy Wijaya <143471727+dehendywijay@users.noreply.github.com>
2026-04-28 21:58:23 +0700

    feat:add services get review

--- backend/services/review_service.go
+++ backend/services/review_service.go
@@ -2,10 +2,42 @@ package services
 
 import (
 	"triptix/config"
+	"triptix/dto"
 	"triptix/models"
 )
 
-func CreateReview(review models.Review) (models.Review, error) {
+
+func CreateReview(req dto.CreateReviewRequest) (dto.ReviewResponse, error) {
+	review := models.Review{
+		UserID:   req.UserID,
+		WisataID: req.WisataID,
+		Rating:   req.Rating,
+		Comment:  req.Comment,
+	}
+
 	err := config.DB.Create(&review).Error
-	return review, err
+	if err != nil {
+		return dto.ReviewResponse{}, err
+	}
+
+	return dto.ReviewResponse{
+		ID:       review.ID,
+		UserID:   review.UserID,
+		WisataID: review.WisataID,
+		Rating:   review.Rating,
+		Comment:  review.Comment,
+	}, nil
+}
+
+
+func GetReviewsByWisataID(wisataID uint) ([]models.Review, error) {
+	var reviews []models.Review
+
+	err := config.DB.
+		Preload("User").
+		Preload("Wisata").
+		Where("wisata_id = ?", wisataID).
+		Find(&reviews).Error
+
+	return reviews, err
 }
