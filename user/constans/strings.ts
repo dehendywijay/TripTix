2cfc6289275a2ef316c524348d92f825ae5535bf
dehendyw <dehendywijaya2@gmail.com>
2026-04-29 14:01:08 +0700

    feat: add reusable api

---
+++ user/constans/strings.ts
@@ -0,0 +1,12 @@
+export const api = 'http://localhost:8080/api';
+
+export const api_wisata = `${api}/wisata`;
+
+
+export const api_user = `${api}/user`;
+export const api_user_register = `${api_user}/register`;
+export const api_user_login = `${api_user}/login`;
+export const api_user_logout = `${api_user}/logout`;
+
+export const api_reviews = `${api}/reviews`;
+export const api_reviews_by_wisata = `${api_reviews}/wisata/:id`;
