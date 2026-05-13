0c01d5c7bbd1fd755158d85260a17a4b555902d2
Dehendy Wijaya <143471727+dehendywijay@users.noreply.github.com>
2026-05-13 14:26:26 +0700

    minor: membuat respone untuk data

--- user/type/wisata.ts
+++ user/type/wisata.ts
@@ -2,10 +2,12 @@ export type Wisata = {
     id: string;
     nama: string;
     alamat: string;
-    deskripsi: string;
-    durasi: number;
-    jenis: string;
     harga: number;
     kategori: string;
     foto : string
+};
+
+export type WisataResponse = {
+  id: any;
+  data: Wisata[];
 };
