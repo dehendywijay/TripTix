49b7bdd4d75e71da4d0686344c4d4c793c18d822
Dehendy Wijaya <143471727+dehendywijay@users.noreply.github.com>
2026-05-13 13:56:53 +0700

    feat: membuat type untuk wisata

---
+++ user/type/wisata.ts
@@ -0,0 +1,11 @@
+export type Wisata = {
+    id: string;
+    nama: string;
+    alamat: string;
+    deskripsi: string;
+    durasi: number;
+    jenis: string;
+    harga: number;
+    kategori: string;
+    foto : string
+};
