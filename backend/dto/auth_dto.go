68d8d8b4cd2910b892ee17a665cbc94e531037ff
Dehendy Wijaya <143471727+dehendywijay@users.noreply.github.com>
2026-05-14 16:49:36 +0700

    feat: login request

--- backend/dto/auth_dto.go
+++ backend/dto/auth_dto.go
@@ -7,6 +7,11 @@ type LoginRespone struct {
 	AccesToken string `json:"access_token"`
 }
 
+type LoginRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
 type RefreshTokenRequest struct {
 	RefreshToken string `json:"refresh_token"`
 }
